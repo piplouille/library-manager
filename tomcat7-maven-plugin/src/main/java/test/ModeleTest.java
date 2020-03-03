569745f4ce7e36851f58483095141a7032f82b17
DEBESSE Laetitia <laetitia.debesse@ensta-paristech.fr>
2020-03-03 11:40:21 +0100

    ex1 ModeleTest

---
+++ tomcat7-maven-plugin/src/main/java/test/ModeleTest.java
@@ -0,0 +1,12 @@
+package test;
+
+import model.Abonnement;
+import model.Emprunt;
+import model.Livre;
+import model.Membre;
+
+public class ModelTest {
+    public static void main() {
+        Membre madeleine = new Membre(0, "Becker", "Madeleine", "1024 Bvd des Maréchaux", "madeleine@ensta.fr", "0600", Abonnement.VIP);
+    }
+}
