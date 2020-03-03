553a6d59348f3f6a5b23dcf78c3459803862eb5f
DEBESSE Laetitia <laetitia.debesse@ensta-paristech.fr>
2020-03-03 11:47:31 +0100

    Fin de tests exercice 1

--- tomcat7-maven-plugin/src/main/java/test/ModeleTest.java
+++ tomcat7-maven-plugin/src/main/java/test/ModeleTest.java
@@ -1,5 +1,7 @@
 package test;
 
+import java.time.LocalDate;
+
 import model.Abonnement;
 import model.Emprunt;
 import model.Livre;
@@ -8,5 +10,7 @@ import model.Membre;
 public class ModeleTest {
     public static void main() {
         Membre madeleine = new Membre(0, "Becker", "Madeleine", "1024 Bvd des Maréchaux", "madeleine@ensta.fr", "0600", Abonnement.VIP);
+        Livre roman = new Livre(0, "Le Petit Prince", "Antoine de St Exupery", "9791187192596");
+        Emprunt emprunt = new Emprunt(0, madeleine, roman, LocalDate.now(), LocalDate.of(2020, 4, 3));
     }
 }
