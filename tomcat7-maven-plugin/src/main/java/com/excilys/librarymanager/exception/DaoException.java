97e45b140b7d70202303f8e9779c73223b4bd24e
DEBESSE Laetitia <laetitia.debesse@ensta-paristech.fr>
2020-03-03 12:14:10 +0100

    ex2 - classe exception

--- tomcat7-maven-plugin/src/main/java/com/excilys/librarymanager/exception/DaoException.java
+++ tomcat7-maven-plugin/src/main/java/com/excilys/librarymanager/exception/DaoException.java
@@ -1,5 +1,8 @@
 package com.excilys.librarymanager.exception;
 
 public class DaoException extends Exception {
-    
+    public DaoException(String msg) {
+        System.out.println(msg);
+        getStackTrace();
+    }
 }
