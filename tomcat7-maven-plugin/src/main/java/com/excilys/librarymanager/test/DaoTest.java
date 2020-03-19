74bd02d65d7feb268ac8d37e7863530db56634a2
DEBESSE Laetitia <laetitia.debesse@ensta-paristech.fr>
2020-03-19 18:54:04 +0100

    ajout dossier

--- tomcat7-maven-plugin/src/main/java/com/excilys/librarymanager/test/DaoTest.java
+++ tomcat7-maven-plugin/src/main/java/com/excilys/librarymanager/test/DaoTest.java
@@ -14,6 +14,7 @@ import java.time.LocalDate;
 
 public class DaoTest {
     public static void main() {
+        Membre laetitia = new Membre(1,"Debesse","Laetitia","1024 Bvd des Marechaux","debesse@ensta.fr","5462",Abonnement.PREMIUM);
         Membre madeleine = new Membre(0, "Becker", "Madeleine", "1024 Bvd des Marechaux", "madeleine@ensta.fr", "0600", Abonnement.VIP);
         Livre roman = new Livre(0, "Le Petit Prince", "Antoine de St Exupery", "9791187192596");
         Emprunt emprunt = new Emprunt(0, madeleine, roman, LocalDate.now(), LocalDate.of(2020, 4, 3));
