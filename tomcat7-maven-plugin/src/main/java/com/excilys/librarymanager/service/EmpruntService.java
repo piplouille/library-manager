e7cd1c8f4c17f3f693a1946e74e0c7c2b88836e4
DEBESSE Laetitia <laetitia.debesse@ensta-paristech.fr>
2020-03-20 11:27:24 +0100

    test ex3

--- tomcat7-maven-plugin/src/main/java/com/excilys/librarymanager/service/EmpruntService.java
+++ tomcat7-maven-plugin/src/main/java/com/excilys/librarymanager/service/EmpruntService.java
@@ -14,7 +14,7 @@ public interface EmpruntService {
 	public List<Emprunt> getListCurrentByLivre(int idLivre) throws ServiceException;
 	public Emprunt getById(int id) throws ServiceException;
 	public void create(int idMembre, int idLivre, LocalDate dateEmprunt) throws ServiceException;
-	public void returnBook(int id) throws ServiceException;
+	public void returnBook(int idEmprunt) throws ServiceException;
 	public int count() throws ServiceException;
 	public boolean isLivreDispo(int idLivre) throws ServiceException;
 	public boolean isEmpruntPossible(Membre membre) throws ServiceException ;
