94f90ad5c86a681f7e81957ef74dc1431db05a8b
DEBESSE Laetitia <laetitia.debesse@ensta-paristech.fr>
2020-03-22 15:03:07 +0100

    LA PREMIERE SERVLET AFFFFIIIICICEHHAHZFJEJFEDZAFEZFEZKFEZGFEZ

--- tomcat7-maven-plugin/src/main/java/com/excilys/librarymanager/servlet/DashboardServlet.java
+++ tomcat7-maven-plugin/src/main/java/com/excilys/librarymanager/servlet/DashboardServlet.java
@@ -1,24 +1,20 @@
 package com.excilys.librarymanager.servlet;
 
-import javax.servlet.http.*;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
-import java.util.*;
-import java.text.*;
-import java.io.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+// import java.util.*;
+// import java.text.*;
+// import java.io.*;
 
-import com.excilys.librarymanager.exception.ServletException;
 import java.io.IOException;
 
 public class DashboardServlet extends HttpServlet {
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        try {
-            this.getServletContext().getRequestDispatcher("/WEB-INF/View/dashboard.jsp").forward(request, response);
-        }
-        catch (IOException e) {
-            throw new ServletException("Erreur : vtff");
-        }
-        catch (Exception e) {
-            throw new IOException("help");
-        }   
+        RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/View/dashboard.jsp");
+        dispatcher.forward(request, response);
+        // this.getServletContext().getRequestDispatcher("/WEB-INF/View/dashboard.jsp").forward(request, response);
     }
 }
