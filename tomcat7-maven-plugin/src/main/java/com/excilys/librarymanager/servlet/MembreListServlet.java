079572e81cb29ecde89f1decef19699e35776a13
DEBESSE Laetitia <laetitia.debesse@ensta-paristech.fr>
2020-03-23 20:16:52 +0100

    heu

--- tomcat7-maven-plugin/src/main/java/com/excilys/librarymanager/servlet/MembreListServlet.java
+++ tomcat7-maven-plugin/src/main/java/com/excilys/librarymanager/servlet/MembreListServlet.java
@@ -11,12 +11,17 @@ import javax.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
 
+import com.excilys.librarymanager.model.Membre;
+import com.excilys.librarymanager.service.MembreServiceImpl;
+
 public class MembreListServlet extends HttpServlet {
     /*
     Une méthode :
     doGet() : afficher membres
     */
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        
+        
         RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/View/membre_list.jsp");
         dispatcher.forward(request, response);
     }
